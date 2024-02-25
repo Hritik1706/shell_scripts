f9792ec5bac0951b5e52b04bea1b6229525a4804
Hritik Doud <127307238+Hritik1706@users.noreply.github.com>
2024-02-25 20:17:07 +0530

    Add files via upload

---
+++ info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#this is comment
+
+echo "hritik here "
+echo "get my ip address"
+read input
+echo " i running my first  $input  "
+echo "time from server is running "
+uptime
+echo "skill you learn  $@"
+
